7e7314742498e9eaa1bde5ff3be37b21391fe0c3
Mohammed's School Account <159501876+mcodes450@users.noreply.github.com>
2026-05-27 00:47:53 -0400

    Update PlayerStream.hpp

--- PlayerStream.hpp
+++ PlayerStream.hpp
@@ -11,23 +11,9 @@
  */
 class PlayerStream {
 public:
-    // Student Note: Added a virtual destructor to ensure proper cleanup of derived classes.
-    virtual ~PlayerStream() = default;
-
-    /**
-     * @brief Retrieves the next Player in the stream, if possible.
-     * @return The next Player object in the sequence.
-     * @post Updates stream members so a subsequent call to
-     *      nextPlayer() will yield the next Player in the to-be-read sequence.
-     * @throws std::runtime_error, if there are no more players to fetch & nextPlayer() is called.
-     */
     virtual Player nextPlayer() = 0;
-
-    /**
-     * @brief Returns the number of players remaining in the stream.
-     * @return The count of players left to be read.
-     */
     virtual size_t remaining() const = 0;
+    virtual ~PlayerStream() = default;
 };
 
 /**
@@ -35,28 +21,22 @@ public:
  */
 class VectorPlayerStream : public PlayerStream {
 private:
-    // Student Design Choice: Using a vector to store the stream data and a size_t 
-    // to track our current position. This allows O(1) access to the next player.
     std::vector<Player> players_;
-    size_t current_index_;
+    size_t cursor_;
 
 public:
     /**
      * @brief Constructs a VectorPlayerStream from a vector of Players.
-     * @param players The vector of Player objects to stream.
      */
     VectorPlayerStream(const std::vector<Player>& players);
 
     /**
     * @brief Retrieves the next Player in the stream.
-    * @return The next Player object in the sequence.
-    * @throws std::runtime_error If there are no more players remaining in the stream.
     */
     Player nextPlayer() override;
 
     /**
      * @brief Returns the number of players remaining in the stream.
-     * @return The count of players left to be read.
      */
-    size_t remaining() const override; 
+    size_t remaining() const override;
 };
