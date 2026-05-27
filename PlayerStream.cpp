2711ef1844538660eea78f50186e67e37c3d0213
Mohammed's School Account <159501876+mcodes450@users.noreply.github.com>
2026-05-27 01:08:34 -0400

    Update PlayerStream.cpp

--- PlayerStream.cpp
+++ PlayerStream.cpp
@@ -1,11 +1,20 @@
 #include "PlayerStream.hpp"
 
+// The constructor sets up our streaming interface by copying the vector input.
+// I am tracking the current position using a dedicated size_t index variable,
+// initializing it to zero so that the stream always begins reading from the
+// very first element of the underlying container.
 VectorPlayerStream::VectorPlayerStream(const std::vector<Player>& players)
     : players_(players)
     , current_index_(0)
 {
 }
 
+// This function acts as an iterator that advances through our player records.
+// First, I need a guard clause to check if our tracking index has hit or exceeded
+// the total size of the vector, which would mean there are no more players to read.
+// If it is out of bounds, I throw a runtime_error immediately to prevent unsafe memory access;
+// otherwise, I return the current player and post-increment the index to advance the stream.
 Player VectorPlayerStream::nextPlayer()
 {
     if (current_index_ >= players_.size()) {
@@ -14,6 +23,10 @@ Player VectorPlayerStream::nextPlayer()
     return players_[current_index_++];
 }
 
+// To determine how many elements are left unread, I am using simple subtraction.
+// By subtracting our current position index from the overall size of the vector,
+// we get an instantaneous, constant-time count of the remaining objects,
+// which helps the main loops safely evaluate termination conditions.
 size_t VectorPlayerStream::remaining() const
 {
     return players_.size() - current_index_;
